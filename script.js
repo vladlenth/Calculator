250c4c06e617f87a132a897c99b4995a1b49f8cf
vladlenth <vladbass445@gmail.com>
2024-11-25 22:35:04 +0300

    refactoring

--- script.js
+++ script.js
@@ -1,64 +1,62 @@
-const result = document.querySelector('#result'),
-      expression = document.querySelector('#expression'),
-      num = document.querySelectorAll('.number:not(.equals)'),
-      operation = document.querySelectorAll('.operation'),
-      equals = document.querySelector('.equals'),
-      clear = document.querySelector('#clear'),
-      ce = document.querySelector('#ce');
-let ex = ''; // the expression string to be eval'd
-result.innerHTML = '0';
+const result = document.querySelector('#result');
+const expression = document.querySelector('#expression');
+const num = document.querySelectorAll('.number:not(.equals)');
+const operation = document.querySelectorAll('.operation');
+const equals = document.querySelector('.equals');
+const clear = document.querySelector('#clear');
+const ce = document.querySelector('#ce');
 
+let ex = ''; // expressionString
+result.innerHTML = '0'; // first result = 0
 
-
-function clickN() { // when we click on a number
-  if(!ex || typeof(ex) === 'number' || ex === '0') {
+// click Number
+function clickN() {
+  if (!ex || ex === '0') {
     expression.innerHTML = this.id;
     ex = this.id;
   } else {
     expression.innerHTML += this.id;
     ex += this.id;
   }
-  result.innerHTML = ex.split(/\/|\*|\+|-|=/).pop();
-  checkLength(result.innerHTML);
-};
+  const lastValue = ex.split(/\/|\*|\+|-|=/).pop();
+  result.innerHTML = lastValue;
+  checkLength(lastValue);
+}
 
-function clickO() { // when we click on an operation
-  if(!ex) {
-    return;
-  }
+// clickO
+function clickO() {
+  if (!ex) return;
   ex = ex.toString().replace(/=/, '');
   if (ex.match(/\/|\*|\+|-|=/)) {
     ex = eval(ex).toString();
   } 
-  expression.innerHTML = expression.innerHTML.replace(/=/, '') + this.id;
+  expression.innerHTML += this.id;
   ex += this.id;
   result.innerHTML = this.id;
-};
+}
 
+// listener`s
+for (let i = 0; i < num.length; i++) {
+  num[i].addEventListener('click', clickN);
+}
 
+for (let i = 0; i < operation.length; i++) {
+  operation[i].addEventListener('click', clickO);
+}
 
-Array.from(num).forEach(function(element) { // assign appropriate function to all numbers and operations
-      element.addEventListener('click', clickN);
-    });
-
-Array.from(operation).forEach(function(element) {
-      element.addEventListener('click', clickO);
-    });
-
-// clear all on click
+// Clear
 clear.addEventListener('click', () => {
   result.innerHTML = '';
   expression.innerHTML = '';
   ex = '';
-})
+});
 
-// clear last entry on click
+// del last
 ce.addEventListener('click', () => {
   if (!expression.innerHTML.match(/=$/)) {
-    
     expression.innerHTML = doCE(expression.innerHTML);
     ex = doCE(ex); 
-    result.innerHTML = 0;
+    result.innerHTML = '0';
     
     function doCE(arg) {
       arg = arg.split(/([\/\*\+\-\=])/g);
@@ -66,20 +64,21 @@ ce.addEventListener('click', () => {
       return arg.join('');
     }
   }
-})
+});
 
-// calculate the whole thing
-equals.addEventListener('click', ()=> {
+// equals
+equals.addEventListener('click', () => {
   if (!ex) {
     result.innerHTML = '0';
   } else {
     ex = eval(ex);
     expression.innerHTML += '=';
-    result.innerHTML = trim12(ex);
+    result.innerHTML = trim9(ex);
   }
-})
+});
 
-function checkLength(arg) { // if we enter a number that's too long 
+// check length
+function checkLength(arg) {
   if (arg.toString().length > 9) {
     expression.innerHTML = 'number too long'.toUpperCase();
     result.innerHTML = '0';
@@ -87,12 +86,13 @@ function checkLength(arg) { // if we enter a number that's too long
   } 
 }
 
-function trim12(arg) { // if we calculate a number that's too long
-  if (arg.toString().length > 14) {
-    ex = parseFloat(arg.toPrecision(12));
-    if (ex.toString().length > 14) { 
+// trim num
+function trim9(arg) {
+  if (arg.toString().length > 9) {
+    ex = parseFloat(arg.toPrecision(9));
+    if (ex.toString().length > 9) { 
       ex = ex.toExponential(9);
-    };
+    }
     return ex;
   } else {
     return arg;
